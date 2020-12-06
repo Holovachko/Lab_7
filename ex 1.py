4e9f178ecf5d976d1ff77a894f84c9b811c3e984
Holovachko <74302993+Holovachko@users.noreply.github.com>
2020-12-06 18:09:21 +0200

    Update ex 1.py

--- ex 1.py
+++ ex 1.py
@@ -1,23 +1,14 @@
-n = int(input('n = '))
-m = int(input('m = '))
 matrix = []
-num = 1
-q = 0
-for i in range(n):
+i = int(input('Кількість рядків = '))
+j = int(input('кількість стовпців = '))
+for m in range(i):
     b = []
-    for j in range(m):
-        if (j * i) % 2 == 0:
-            for r in range(m+1):
-                num *= r
-            b.append(num)
-        else:
-            for r in range (n + 1):
-                q += r
-            b.append(q)
+    for m in range(j):
+        b.append(float(input('Введіть елементи матриці ')))
     matrix.append(b)
-print(matrix)
-
-list1 = []
-for k in matrix:
-    list1.extend(k)
-print(list1)
+s = 0
+for n in range(i):
+    for m in range(j):
+        if n%2==0 and  m%2==0 and matrix[n][m]<0:
+            s+= matrix[n][m]
+print(s)
